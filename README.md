4c6d4a3436b2e0b4f1c3c4e5c1d2f4164f6a2092
Nanjiang Shu <nanjiang.shu@bils.se>
2017-10-02 22:15:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#dockercommon
+
+Define base Docker images for various OS, so that the docker container can be
+started with custom userid and the home folder is also owned by that user
---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+
+USER_ID=${USER_ID:-1000}
+
+echo "Starting with UID : $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+export HOME=/home/user
+
+chown -R user:user /home/user
+exec /usr/local/bin/gosu user "$@"
+
